cda21c68b2d0bb4e7d84ff193c9614e5e0861df2
takahiro0326 <161424302+takahiro0326@users.noreply.github.com>
2024-03-27 17:36:47 +0900

    ユーザー管理機能

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,11 +3,16 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+         validates :nickname, presence: true
+         validates :family_name, presence: true, format: { with: /\A[^\x20-\x7E｡-ﾟ]+\z/, message: 'は全角文字で入力してください。' }
+         validates :first_name, presence: true, format: { with: /\A[^\x20-\x7E｡-ﾟ]+\z/, message: 'は全角文字で入力してください。' }
+         validates :family_name_kana, presence: true, format: { with: /\A[ァ-ヶー－]+\z/, message: 'は全角カタカナで入力してください。' }
+         validates :first_name_kana, presence: true, format: { with: /\A[ァ-ヶー－]+\z/, message: 'は全角カタカナで入力してください。' }
+         validates :birth_day, presence: true
+       
+         VALID_PASSWORD_REGEX = /\A(?=.*?[a-zA-Z])(?=.*?\d)[a-zA-Z\d]+\z/
+         validates :password, format: { with: VALID_PASSWORD_REGEX, message: 'は半角英数字混合で入力してください。' }
 
-  validates :nickname, presence: true
-  validates :family_name, presence: true
-  validates :first_name, presence: true
-  validates :family_name_kana, presence: true, format: { with: /\A[\p{katakana}\p{blank}ー－]+\z/, message: "は全角カタカナで入力してください" }
-  validates :first_name_kana, presence: true, format: { with: /\A[\p{katakana}\p{blank}ー－]+\z/, message: "は全角カタカナで入力してください" }
-  validates :birth_day, presence: true
+         has_many:products
+         has_many:purchase_histories
 end
