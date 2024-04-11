3962281f6986b0d6722ec8c6e92c81338be86a9a
takahiro0326 <161424302+takahiro0326@users.noreply.github.com>
2024-04-11 19:45:50 +0900

    商品購入機能

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,6 +1,7 @@
 class ProductsController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
   before_action :set_product, only: [:show, :edit, :update, :destroy]
+  before_action :move_to_index, except: [:index, :new, :create, :show, :destroy]
 
   def index
     @products = Product.order(created_at: :desc)
@@ -51,5 +52,9 @@ class ProductsController < ApplicationController
 
   def product_params
     params.require(:product).permit(:product_name, :description_item, :price, :image, :category_id, :condition_id, :shipping_charge_id, :prefecture_id, :shipping_date_id).merge(user_id: current_user.id)
+  end
+
+  def move_to_index
+    redirect_to root_path if current_user.id != @product.user.id || Order.exists?(product_id: @product.id)
   end
 end
